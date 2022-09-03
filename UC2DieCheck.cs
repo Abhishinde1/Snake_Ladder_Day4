04515979667bf731440fa72232ac5b6a6e20d3ea
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-03 15:45:08 +0530

    DieCheck Program Added

---
+++ UC2DieCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    internal class UC2DieCheck
+    {
+        public static void dieCheck()
+        {
+            int START_POSITION = 0;
+
+            //Die Check
+            Random random = new Random();
+            int dieCheck = random.Next(0, 7);
+            if (dieCheck == 0)
+            {
+                dieCheck = dieCheck + 1;
+
+            }
+            Console.WriteLine("Number on die is: " + dieCheck);
+        }
+    }
+}
