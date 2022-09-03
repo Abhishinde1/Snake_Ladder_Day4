e17f2d55366db4f5b6558735ec71b8a377f81b5f
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-03 15:43:44 +0530

    StartingPosition prog Added

---
+++ UC1Startingpoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+     class UC1Startingpoint
+    {
+        public static void startPosition()
+        {
+            int START_POSITION = 0;
+
+            Console.WriteLine("Welcome to snake ladder game");
+            Console.WriteLine("Player will start from zero position");
+        }
+
+    }
+}
