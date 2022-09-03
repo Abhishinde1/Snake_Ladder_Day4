6ccfdf15872ec7627eeb1d6ec2bfc27f4ca32269
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-03 15:42:10 +0530

    Snake Ladder Program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace snake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Program");
+
+            Console.WriteLine("Enter the program number \n 1. Enter the satrting position  2. Check the number on die");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                  
+                  UC1Startingpoint.startPosition();
+                    break;
+                case 2:
+                    UC2DieCheck.dieCheck(); 
+                    break;
+            }
+        }
+               
+        }
+    }
