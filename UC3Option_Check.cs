0a7579b3bd64bdc3c33372be2d930195b5180774
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-03 16:06:29 +0530

    OptionCheck Program Added

---
+++ UC3Option_Check.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class UC3Option_Check
+    {
+        static int START_POSITION = 0;
+
+
+        public static void Checkoption()
+        {
+
+            int currentPlayerPosition = 0;
+            int position = 0;
+
+            Console.WriteLine("Welcome to snake ladder game");
+            Console.WriteLine("Player will start from zero position");
+
+            Random random = new Random();
+            int dieRolling = random.Next(0, 7);
+            if (dieRolling == 0)
+            {
+                dieRolling = dieRolling + 1;
+
+            }
+            Console.WriteLine("Number on die is: " + dieRolling);
+            position = currentPlayerPosition + dieRolling;
+
+            Random randomNum = new Random();
+            int checkOptions = randomNum.Next(0, 3);
+            switch (checkOptions)
+            {
+
+                case 0:
+                    {
+                        Console.WriteLine("There will be no play. Player will be at same position");
+                    }
+
+                    break;
+
+                case 1:
+                    {
+                        Console.WriteLine("Player will move forward");
+                        if (currentPlayerPosition < 100)
+                        {
+                            currentPlayerPosition = position;
+                        }
+                    }
+                    Console.WriteLine("Player is at: " + currentPlayerPosition);
+                    break;
+
+                case 2:
+                    {
+                        Console.WriteLine("Player is bitten by a snake");
+                        if (currentPlayerPosition > 0)
+                        {
+                            currentPlayerPosition = position - dieRolling;
+
+                        }
+                        else if (currentPlayerPosition <= 0)
+                        {
+                            currentPlayerPosition = START_POSITION;
+                        }
+                    }
+                    Console.WriteLine("Player is at: " + currentPlayerPosition);
+                    break;
+            }
+
+
+        }
+    }
+}
+
